af9d3ae7f0bc809b8c5e3e14064ae35ff9ad920a
Rayen Troudi <94689247+kugeIblitz@users.noreply.github.com>
2023-05-20 10:00:17 +0100

    Database connection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.miniprojetfx {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
+
+
     opens com.rayen.miniprojetfx to javafx.fxml;
     exports com.rayen.miniprojetfx;
 }
